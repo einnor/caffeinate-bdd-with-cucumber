af3fbd0e3a19740920a73aac24358934cc9beb91
Ronnie Nyaga <ronnienyaga@microsoft.com>
2021-02-17 11:59:39 +0300

    Added step definitions and minimum code to pass the tests

---
+++ features/stepDefintions/steps.js
@@ -0,0 +1,22 @@
+const { Given, When, Then, Before } = require('@cucumber/cucumber');
+const assert = require('assert').strict;
+
+const { Customer, Associate } = require('../../lib/Caffeinate');
+
+Before(function () {
+    this.maria = new Customer();
+    this.li = new Associate();
+});
+
+Given('Maria orders ${int} of coffee from Li', function (int) {
+    const price = int;
+    this.maria.order();
+});
+
+When('Maria pays with a credit card', function () {
+    this.maria.paysWithCreditCard();
+});
+
+Then('Li should process the payment', function () {
+    assert.equal(this.li.processPayment(), true);
+});
